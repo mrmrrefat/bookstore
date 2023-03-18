40543173b5eb3303cabc96ae2adcc8035371ce05
nermeen <nermeenhabeel@gmail.com>
2023-03-18 19:50:47 +0200

    crud operation created for order table

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,7 @@ Route::get('/', function () {
 
 // Route::resource('books' , App\Http\Controllers\BookController::class);
 Route::resource('books',\App\Http\Controllers\BookController::class);
+Route::resource('orders',App\Http\Controllers\OrderController::class);
 // Auth::routes();
 // Route::get('/token', function () {
 //     return csrf_token(); 
